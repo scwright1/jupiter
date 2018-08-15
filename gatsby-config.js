0a65f885ac50b064c91af865d54bfe8d8388fe86
Steve Wright <ste.c.wr@gmail.com>
2018-08-15 11:05:05 +0100

    basic setup of gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Stephen C Wright | Technical Producer',
+        author: 'Steve Wright',
+        description: 'Stephen C Wright August 2018 Portfolio',
+        siteUrl: 'https://scwright1.github.io/jupiter',
+    },
+    pathPrefix: '/jupiter',
+}
