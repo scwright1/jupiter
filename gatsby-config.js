70f8156b5889a092a0c9337de8fb117a0cf3328a
Steve Wright <ste.c.wr@gmail.com>
2018-09-04 16:06:45 +0100

    updated system for custom domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,5 @@ module.exports = {
         description: 'Stephen C Wright August 2018 Portfolio',
         siteUrl: 'https://scwright1.github.io/jupiter',
         shortTitle: 'Stephen Wright',
-    },
-    pathPrefix: '/jupiter',
+    }
 }
