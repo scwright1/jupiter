1ea0ec7757f0d06ec42347e86c92f8b527414075
Steve Wright <ste.c.wr@gmail.com>
2018-09-04 16:08:38 +0100

    Updates

---
+++ 2-c2d57af4fed29e86800e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{147:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"sq--src-components-logo-js":95422705,index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=2-c2d57af4fed29e86800e.js.map
--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    siteMetadata: {
-        title: 'Stephen C Wright | Technical Producer',
-        author: 'Steve Wright',
-        description: 'Stephen C Wright August 2018 Portfolio',
-        siteUrl: 'https://scwright1.github.io/jupiter',
-        shortTitle: 'Stephen Wright',
-    }
-}
