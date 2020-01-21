411bdefe38e15f9e40e1b557d0a55f69c691839a
Steve Wright <ste.c.wr@gmail.com>
2020-01-21 22:58:31 +0000

    reset master and commit new landing page

--- 2-b6fcc4500e3430d88afa.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-components-layouts-index-layout-js":2729156960,"sq--src-components-logo-js":95422705,"sq--src-components-panels-blog-js":1639395404}}}}]);
-//# sourceMappingURL=2-b6fcc4500e3430d88afa.js.map
